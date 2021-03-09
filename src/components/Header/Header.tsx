b3843b5036a8e4575e845780d7aa1e6a974814c1
D'Arcy Myronuk <darcy.myronuk@gmail.com>
2021-03-08 23:01:44 -0500

    initial commit - add react, tailwindcss, eslint

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header className="border-b p-4 bg-gray-50">
+    <h1 className="text-3xl">Forex Dashboard</h1>
+  </header>
+)
+
+export default Header
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Main = () => (
+  <main className="bg-gray-100" />
+)
+
+export default Main
